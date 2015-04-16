0debfe382576c2a43c076f8c8104529efbd60ce0
janejl <jane.hkucs@gmail.com>
2015-04-16 10:18:31 +0800

    cannot run, compare it with previous version and find error!

--- part2/rdt-part2.h
+++ part2/rdt-part2.h
@@ -109,10 +109,14 @@ typedef struct PACKET{
 	u32b_t sequence;
 	u16b_t checksum;
 	char payload[PAYLOAD]; // ACK packet has 0 payload
-} Packet;
+}Packet;
 int currentSequence = 0; // 0 or 1
 int recvSequence = 0; // 0 or 1, expected receiving sequence
 int lastSequence = 1;
+int lastACKnum = 1;
+
+Packet makeDataPacket(u32b_t seq, char *payload, int length);
+Packet makeACKPacket(u32b_t seq);
 
 int rdt_socket();
 int rdt_bind(int fd, u16b_t port);
@@ -120,10 +124,6 @@ int rdt_target(int fd, char * peer_name, u16b_t peer_port);
 int rdt_send(int fd, char * msg, int length);
 int rdt_recv(int fd, char * msg, int length);
 int rdt_close(int fd);
-
-u16b_t packetChecksum(Packet packet, int length);
-Packet makeDataPacket(u32b_t seq, char *payload, int length);
-Packet makeACKPacket(u32b_t seq);
 
 /* Application process calls this function to create the RDT socket.
    return	-> the socket descriptor on success, -1 on error 
@@ -179,34 +179,6 @@ int rdt_target(int fd, char * peer_name, u16b_t peer_port){
     return 0;
 }
 
-/* An assisting funtion to check the checksum for the whole packet
- * including both types of packets.
- * length -- total length (byte) of packet
- */
-u16b_t packetChecksum(Packet *pkt, int length){
-	char *packBuff, *typeBuff, *seqBuff, *checkBuff;
-	packBuff = (char *)malloc((PAYLOAD+10) * sizeof(char));
-	typeBuff = (char *)malloc(4 * sizeof(char));
-	seqBuff = (char *)malloc(4 * sizeof(char));
-	checkBuff = (char *)malloc(2 * sizeof(char));
-
-	sprintf(typeBuff, "%u", pkt->type);
-	sprintf(seqBuff, "%u", pkt->sequence);
-	sprintf(checkBuff, "%hu", pkt->checksum);
-
-	if(pkt->type == 1){
-		// for data packet
-		sprintf(packBuff, "%s%s%s%s", typeBuff, seqBuff, checkBuff, pkt->payload);
-	}else{
-		// for ACK packet
-		sprintf(packBuff, "%s%s%s", typeBuff, seqBuff, checkBuff);
-	}
-
-	u16b_t result = checksum((u8b_t *)packBuff, (u16b_t)length);
-
-	return result;
-}
-
 Packet makeDataPacket(u32b_t seq, char *payload, int length){
 	Packet pkt;
 	pkt.type = 1;
@@ -232,82 +204,92 @@ Packet makeACKPacket(u32b_t seq){
 */
 int rdt_send(int fd, char * msg, int length){
 //implement the Stop-and-Wait ARQ (rdt3.0) logic
-	//printf("rdt_send start...");
-
-	/*
-	 * Packet makeDataPacket(u32b_t seq, char *payload, int length);
-	 * Packet makeACKPacket(u32b_t seq);
-	 */
 
 	Packet sendpkt = makeDataPacket(currentSequence, msg, length);
 	sendpkt.checksum = checksum((u8b_t *)&sendpkt, sizeof sendpkt);
-	Packet recvpkt;
 
-	printf("[rdt_send]udt_send msn packet... seq=%d\n", sendpkt.sequence);///
 	udt_send(fd, &sendpkt, sizeof sendpkt, 0);
+	printf("[rdt_send] send datapkt..... seq=%d, size=%ld\n", sendpkt.sequence, strlen(sendpkt.payload));///
 
 	fd_set master, read_fds;
 	struct timeval timer; // for timeout
-	int status, nbytes, fdmax;
-	u16b_t checkvalue;
+	int status;
 
 	FD_ZERO(&master);
 	FD_ZERO(&read_fds);
     FD_SET(fd, &master);
-    fdmax = fd;
 
 	while(1){
 		timer.tv_sec = 0;
 		timer.tv_usec = TIMEOUT;
 		read_fds = master;
-		status = select(fdmax+1, &read_fds, NULL, NULL, &timer);
+		status = select(fd+1, &read_fds, NULL, NULL, &timer);
 
 		if(status == -1){
-			perror("[rdt_send]select:");
+			perror("[rdt_send] select:");
 			return -1;
 		}else if(status == 0){
 			// Timeout: retransmit
-			printf("[rdt_send]timeout, retransmit... seq=%d\n", sendpkt.sequence);
 			udt_send(fd, &sendpkt, sizeof sendpkt, 0);
+			printf("[rdt_send] timeout:resend... seq=%u\n", sendpkt.sequence);////
+
 		}else if(status > 0){
-			if(!FD_ISSET(fd, &read_fds)){
-				FD_SET(fd, &master);
-				continue;
+			// packet arrive
+			Packet recvpkt;
+			int nbytes = recv(fd, &recvpkt, sizeof recvpkt, 0);
+			printf("[rdt_send] receive pkt...... seq=%u, type=%u, size=%lu\n", recvpkt.sequence, recvpkt.type, strlen(recvpkt.payload));///
+
+			if(nbytes <= 0){
+				printf("[rdt_send] got error or connection closed by client.\n");
+				return -1;
 			}else{
-				// packet arrive
-				nbytes = recv(fd, &recvpkt, sizeof recvpkt, 0);
+				// got packet from peer
+				u16b_t checkvalue = checksum((u8b_t *)&recvpkt, nbytes);
 
-				if(nbytes <= 0){
-					// got error or connection closed by client
-					if(nbytes == 0) printf("[rdt_send()]nbytes=0");
-					else perror("[rdt_send()]recv:");
-					return -1;
-				}else{
-					// got packet from peer
-					printf("[rdt_send]recv: get a packet.\n");///
-					checkvalue = checksum((u8b_t *)&recvpkt, sizeof recvpkt);
+				/*
+				if((checkvalue==0)&&(recvpkt.type==2)&&(recvpkt.sequence == currentSequence)){
+					// got expected ackpkt from peer
+					printf("[rdt_send]expected ACK!!!\n");///
 
-					if((checkvalue==0)&&(recvpkt.type==2)&&(recvpkt.sequence == currentSequence)){
+					lastACKnum = currentSequence;
+					currentSequence = abs(currentSequence - 1); // 0 or 1
+					return length;
+				}else if((checkvalue==0)&&(recvpkt.type==1)){
+					// got datapkt from peer: ack it (especially in handshaking)
+					Packet ackpkt = makeACKPacket(recvpkt.sequence);
+					ackpkt.checksum = checksum((u8b_t *)&ackpkt, sizeof ackpkt);
+
+					udt_send(fd, &ackpkt, sizeof ackpkt, 0);
+					printf("[rdt_send] get datapkt:ack...seq=%u\n", ackpkt.sequence);///
+				}*/
+
+				if(checkvalue==0){
+					//packet not corrupt: check type
+					if((recvpkt.type == 2)&&(recvpkt.sequence == currentSequence)){
+						//ackpkt
 						// got expected ackpkt from peer
-						break;
-					}
-					else if((checkvalue==0)&&(recvpkt.type==1)){
-						// got datapkt from peer: ack it to avoid deadlock
-						printf("[rdt_send]recv: get msn packet: ack it to avoid deadlock\n");///
+						printf("[rdt_send]expected ACK!!!\n");///
+
+						lastACKnum = currentSequence;
+						currentSequence = abs(currentSequence - 1); // 0 or 1
+						return length;
+					}else if(recvpkt.type == 1){
+						// got datapkt from peer: ack it (especially in handshaking)
 						Packet ackpkt = makeACKPacket(recvpkt.sequence);
 						ackpkt.checksum = checksum((u8b_t *)&ackpkt, sizeof ackpkt);
-						printf("[rdt_send]recv: send the ackpkt\n");///
+
 						udt_send(fd, &ackpkt, sizeof ackpkt, 0);
+						printf("[rdt_send] get datapkt:ack...seq=%u\n", ackpkt.sequence);///
+					}else{
+						printf("[rdt_send] wrong ackpkt...\n");
 					}
+				}else{
+					printf("[rdt_send] received pkt is corrupt...\n");
 				}
+
 			}
 		}
 	}
-	printf("[rdt_send]expected ACK comes...");///
-
-	// Only when the expected ACK comes will this be executed.
-	currentSequence = abs(currentSequence - 1); // 0 or 1
-	return length;
 }
 
 /* Application process calls this function to wait for a message 
@@ -320,112 +302,104 @@ int rdt_send(int fd, char * msg, int length){
 */
 int rdt_recv(int fd, char * msg, int length){
 	//implement the Stop-and-Wait ARQ (rdt3.0) logic
-	int nbytes;
-	u16b_t checkvalue;
-
-	Packet ackpkt = makeACKPacket(recvSequence);
-	ackpkt.checksum = checksum((u8b_t *)&ackpkt, sizeof ackpkt); // it has header only
-	Packet recvpkt;
 
 	while(1){
-		nbytes = recv(fd, &recvpkt, sizeof recvpkt, 0);
+		Packet recvpkt;
+		int nbytes = recv(fd, &recvpkt, sizeof recvpkt, 0);
 
 		if(nbytes <= 0){
-			// got error or connection closed by client
-			if(nbytes == 0) printf("[rdt_recv()]nbytes=0");
-			else perror("[rdt_recv()]recv:");
-
+			printf("[rdt_recv] got error or connection closed by client.\n");
 			return -1;
 		}else{
 			// got data from peer
-			checkvalue = checksum((u8b_t *)&recvpkt, sizeof recvpkt);
+			u16b_t checkvalue = checksum((u8b_t *)&recvpkt, sizeof recvpkt);
 
 			if(checkvalue != 0){
 				// corrupted, send duplicate ACK
-				printf("[rdt_recv]packet received is corrupted: send duplicate ACK\n");///
+				Packet ackpkt = makeACKPacket(lastSequence);
+				ackpkt.checksum = checksum((u8b_t *)&ackpkt, sizeof ackpkt);
 
-				ackpkt.sequence = lastSequence;
 				udt_send(fd, &ackpkt, sizeof ackpkt, 0);
+				printf("[rdt_recv] corrupted packet: resend ACK...seq=%u\n", ackpkt.sequence);////
+
 			}else if(recvpkt.type == 1){
-				printf("[rdt_recv]received a datapkt...... last & expect seq: %d & %d\n", lastSequence, recvSequence);///
+				printf("[rdt_recv] recv a datapkt...last & expect seq=%u & %u\n", lastSequence, recvSequence);///
 
 				// check whether the DATA packet is expected
 				if(recvpkt.sequence == recvSequence){
-					ackpkt.sequence = recvSequence;
-					printf("[rdt_recv]receive expected datapkt and send ack...\n");////
+					Packet ackpkt = makeACKPacket(recvSequence);
+					ackpkt.checksum = checksum((u8b_t *)&ackpkt, sizeof ackpkt);
+
 					udt_send(fd, &ackpkt, sizeof ackpkt, 0);
-					break;
+					printf("[rdt_recv] correct datapkt: send ack......seq=%u\n", ackpkt.sequence);////
+
+					memcpy(msg, &(recvpkt.payload), nbytes); // get massage
+					printf("[rdt_recv] size of received...............siz=%ld\n", strlen(recvpkt.payload));///
+					lastSequence = recvSequence;
+					lastACKnum = recvSequence;
+					recvSequence = abs(recvSequence - 1);// 0 or 1
+
+					return strlen(recvpkt.payload);
 				}else{
-					ackpkt.sequence = lastSequence;
-					printf("[rdt_recv]datapkt is not expected: send duplicate ack...\n");////
+					Packet ackpkt = makeACKPacket(lastSequence);
+					ackpkt.checksum = checksum((u8b_t *)&ackpkt, sizeof ackpkt);
+
 					udt_send(fd, &ackpkt, sizeof ackpkt, 0);
+					printf("[rdt_recv] wrong datapkt: resend ack......seq=%u\n", ackpkt.sequence);////
 				}
 			}else if(recvpkt.type == 2){
-				// may get data due to previous corrupted connection
-				// Discard it and return error.
-				printf("[rdt_recv]receive ackpkt: ignore it and return -1\n");///
-				return -1;
+				// may get ack. Discard it.
+				printf("[rdt_recv] get ackpkt: ignore it.\n");
 			}
 		}
 	}
 
-	memcpy(msg, &(recvpkt.payload), sizeof recvpkt.payload); // get massage
-	lastSequence = recvSequence;
-	recvSequence = abs(recvSequence - 1);// 0 or 1
-
-	return (sizeof recvpkt.payload);
 }
 
 /* Application process calls this function to close the RDT socket.
 */
 int rdt_close(int fd){
 //implement the Stop-and-Wait ARQ (rdt3.0) logic
-// use select() on a UDP socket, the system could easily check the arrival of
-// packet and timeout at the same time.
-	//printf("rdt_close() start...\n");///
-
-	Packet ackpkt = makeACKPacket(lastSequence);
-	ackpkt.checksum = checksum((u8b_t *)&ackpkt, sizeof ackpkt); // it has header only
-	//Packet recvpkt = makePacket(10, 10, (char*) malloc(PAYLOAD * sizeof(char)));//init, '10' is not within range of type and seq
-	Packet recvpkt;
 
 	// Prepare for select(): fd_set.
-	fd_set master;
+	fd_set master, read_fds;
 	struct timeval timer; // for timeout
 	u16b_t checkvalue;
 	int status, nbytes;
 
 	FD_ZERO(&master); // clear the master and temp sets
+	FD_ZERO(&read_fds);
 	FD_SET(fd, &master);
 
-	//printf("[rdt_close]:while-loop start...");///
 	while(1){
-		timer.tv_sec = 0;
+		timer.tv_sec = 10;
 		timer.tv_usec = TWAIT;
-		status = select(fd+1, &master, NULL, NULL, &timer);
+		read_fds = master;
+		status = select(fd+1, &read_fds, NULL, NULL, &timer);
 
 		if(status == 0){
 			// Timeout: close
-			break;
+			printf("CLOSE!\n");
+		    if(close(fd) < 0){
+		    	perror("[rdt_close]close:");
+		    }
+		    return 0;
 		}else if(status > 0){
 			// packet arrival
-			// get packet from readfd and check the corrupted
-			nbytes = recv(fd, &recvpkt, PAYLOAD+10, 0);
+			Packet recvpkt;
+			nbytes = recv(fd, &recvpkt, sizeof recvpkt, 0);
 			checkvalue = checksum((u8b_t *)&recvpkt, nbytes);
 
-			if((nbytes > 0)&&(checkvalue == 0)&&(recvpkt.type == 1)){
+			if((nbytes > 0)&&(checkvalue == 0)&&(recvpkt.type == 1)&&(recvpkt.sequence == lastACKnum)){
 				// resend ACK
-				ackpkt.sequence = lastSequence;
+				Packet ackpkt = makeACKPacket(lastACKnum);
+				ackpkt.checksum = checksum((u8b_t *)&ackpkt, sizeof ackpkt);
+
+				printf("[rdt_close]resend ACK: type=%u, seq=%u\n", ackpkt.type, ackpkt.sequence);////
 				udt_send(fd, &ackpkt, sizeof ackpkt, 0);
 			}
 		}
 	}
-
-	// Only when TWAIT is done will this be executed.
-    if(close(fd) < 0){
-    	perror("[rdt_close]close:");
-    }
-    return 0;
 }
 
 #endif
